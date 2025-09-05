422646ca374515a312d65adacb89ee8b25da24e2
[dyad] <samyunwanarmenia@gmail.com>
2025-09-05 04:19:23 +0400

    [dyad] Переместил глобальные стили макета в `layout.tsx`. - wrote 2 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen font-[family-name:var(--font-geist-sans)]`}
       >
         {children}
         <Toaster /> {/* Add Toaster here */}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PermissionsRequest from "@/components/permissions-request";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-4 sm:p-8 font-[family-name:var(--font-geist-sans)]">
+    <div> {/* Removed min-h-screen and font styles */}
       <PermissionsRequest />
     </div>
   );
