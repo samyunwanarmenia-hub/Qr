dc0976bad502d9f272a74d1293e8d76605af29e3
[dyad] <samyunwanarmenia@gmail.com>
2025-09-05 06:58:48 +0400

    [dyad] Скрыта запись видео, удалены все toast-уведомления и добавлен эффект нажатия кнопки. - wrote 2 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"; // Import Toaster
+// import { Toaster } from "@/components/ui/sonner"; // Удаляем импорт Toaster
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen font-[family-name:var(--font-geist-sans)]`}
       >
         {children}
-        <Toaster /> {/* Add Toaster here */}
+        {/* <Toaster /> */} {/* Удаляем Toaster */}
       </body>
     </html>
   );
